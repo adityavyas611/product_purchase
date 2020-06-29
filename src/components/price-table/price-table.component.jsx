1a57801ab7db8d9c917d9a16c72640fac041b41c
Aditya Vyas <vyasaditya41@yahoo.in>
2020-06-29 19:19:31 +0530

    fix(add): fix formula (#1)
    * fix(chore): change formula using pow instead of xor
    
    * fix(add): toFixed the price

--- src/components/price-table/price-table.component.jsx
+++ src/components/price-table/price-table.component.jsx
@@ -4,11 +4,11 @@ import "./price-table.style.css";
 const PriceTable = ({ seat, product, company }) => {
     const getPrice = () => {
         if (company === 'cellManager') {
-            return 956 * (seat ^ 0.6) + 540;
+            return 956 * Math.pow(seat,0.6) + 540;
         } else if (company === 'microsoftImporter' && (product === 'microStation' || product === 'revit')) {
-            return 707 * (seat ^ 0.7) + 88;
+            return 707 * Math.pow(seat,0.7) + 88;
         } else {
-            return 354 * (seat ^ 0.7) + 44;
+            return 354 * Math.pow(seat,0.7) + 44;
         }
     };
 
@@ -26,7 +26,7 @@ const PriceTable = ({ seat, product, company }) => {
                 </thead>
                 <tbody className="tablebody">
                     <tr>
-                        <td className="borderTable">${price}</td>
+                        <td className="borderTable">${price.toFixed(2)}</td>
                         <td className="borderTable">${(price * 0.3).toFixed(2)}</td>
                         <td className="borderTable">${(price + (price * 0.3)).toFixed(2)}</td>
                     </tr>
